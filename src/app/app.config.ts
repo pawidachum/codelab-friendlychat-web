ccb96d0cf46258487154103bd4b0cb7824f35f5f
pawida <pawida.may@gmail.com>
2024-12-07 09:06:24 +0700

    Adding Rules

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging()),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-1d785","appId":"1:250065285147:web:ee5a4b9459928f57e3c8be","storageBucket":"friendlychat-1d785.firebasestorage.app","apiKey":"AIzaSyB6ETUidio9s-3a7-PB2OFHO0bIhKIgeAk","authDomain":"friendlychat-1d785.firebaseapp.com","messagingSenderId":"250065285147"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
